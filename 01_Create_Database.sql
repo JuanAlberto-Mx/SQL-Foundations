0cbbca161d140a3b58b6657e72602f8d99f926e1
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-03-29 19:21:04 -0600

    Update file content with new descriptions

---
+++ 01_Create_Database.sql
@@ -0,0 +1,7 @@
+-- Create a database to store the information of a company.
+
+CREATE DATABASE company; -- Each SQL statement finishes with semicolon
+
+-- Delete a database.
+
+DROP DATABASE company;
--- 02_Create_Tables.sql
+++ 02_Create_Tables.sql
@@ -1,12 +1,20 @@
--- Creating a table with three colums
+-- Creating a table to contain a company's staff information.
+-- The staff representation consists of the following aspects:
+-- 1) A control number to identify the person.
+-- 2) The full name of the person separated by first and last name.
+-- 3) The age of the person.
+-- 4) The gender of the person.
+
 CREATE TABLE staff ( -- Use the more natural collective term where possible
-	control_number INTEGER, -- Add a coma at the finish of every statement
+	control_number INTEGER, -- Add a coma at the finish of every column specification
 	first_name VARCHAR(20), -- Separate with underscore the names composed by two or more words
-	last_name VARCHAR(20) -- The last statement does not finish with a coma
+	last_name VARCHAR(20),
+    age INTEGER,
+    gender VARCHAR(1) -- The last column specification does not finish with a coma
 );
 
--- Obtaining a table's specification by using the [sp_columns] store procedure
-exec sp_columns users;
+-- Obtaining the staff table's specification by using the [sp_columns] store procedure.
+EXEC sp_columns staff;
 
--- Deleting a table
-drop table users;
+-- Deleting the staff table.
+DROP TABLE staff;
