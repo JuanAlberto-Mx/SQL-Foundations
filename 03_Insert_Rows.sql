5c271b8d6bcb70143d20b673110f7e0280814996
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-03-30 09:42:28 -0600

    Add 03_Insert_Rows.sql initial file with annotations

---
+++ 03_Insert_Rows.sql
@@ -0,0 +1,8 @@
+-- Insert new records in a table specified
+-- INSERT INTO table_name (column1, column2, column...) VALUES(value1, value2, value...);
+-- Columns specification after the table name is optional.
+-- Values section is mandatory and must match each value of the columns specification when exists.
+
+INSERT INTO staff(control_number, first_name, last_name) VALUES(1, 'Juan Alberto', 'Hernandez Martinez', 37, 'M');
+INSERT INTO staff(control_number, first_name, last_name) VALUES(2, 'Ricardo Alberto', 'Hernandez Solis', 29, 'M');
+INSERT INTO staff(control_number, first_name, last_name) VALUES(3, 'Gricelda', 'Solis Lopez', 31, 'F');
