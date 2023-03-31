6f4804a926d3e1be9c083906ed6210f524545f78
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-03-30 18:02:39 -0600

    Add 04_Delete_Rows.sql initial file

---
+++ 04_Delete_Rows.sql
@@ -0,0 +1,4 @@
+-- Delete rows from a specific table
+-- DELETE FROM table_name WHERE condition;
+-- If 'WHERE' statement does not exists, all the records are deleted.
+DELETE FROM positions WHERE position_name = 'Vocalist';
