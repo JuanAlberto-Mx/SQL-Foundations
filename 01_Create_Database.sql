06724d9a21e42414ec8e3c6cf519c554e8c83605
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-04-13 10:45:55 -0600

    Delete files to orginize them in subdirectories

--- 01_Create_Database.sql
+++
@@ -1,5 +0,0 @@
--- Create a database to store the information of musical groups.
-CREATE DATABASE musicdb; -- Each SQL statement finishes with semicolon
-
--- Delete a database.
-DROP DATABASE musicdb;
--- 04_Delete_Rows.sql
+++
@@ -1,4 +0,0 @@
--- Delete rows from a specific table
--- DELETE FROM table_name WHERE condition;
--- If 'WHERE' statement does not exists, all the records are deleted.
-DELETE FROM positions WHERE position_name = 'Vocalist';
